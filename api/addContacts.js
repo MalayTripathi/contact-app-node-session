5efe10d3ebfe772ec4c5aa3dd199569f39b8669c
MalayTripathi <malay_audioslave@hotmail.com>
2017-05-19 23:15:54 +0530

    Button Does Not Work inside Form

--- api/addContacts.js
+++ api/addContacts.js
@@ -1,13 +1,33 @@
 var Contacts = require('../mongoconnect');
 
 saveDataToDB = (req, res, next) => {
-    conData = {
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone
-    }
+   conData = {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone
+	}
 
-    res.json({success: "Success"});
+	Contacts.findOne({ email: conData.email })
+		.then((response) => {
+			if (!response) {
+				console.log('I Am in the API');
+				var contact = new Contacts(conData);
+				contact.save()
+					.then(function (response) {
+						console.log(response);
+					res.send({'Status': "Value Added Successfully"})
+					})
+					.catch(function (err) {
+						res.send({ 'Status': 'Failure', Error: err });
+				})
+			}
+			else {
+				res.send({ 'Status': "Email Already Exists" });
+			}
+		})
+	.catch(function (err) {
+		res.send({ 'Status': err });
+		});
 }
 
 module.exports={
